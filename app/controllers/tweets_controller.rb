aca1dcccf2489a05077dc661e4114f1e041c31b1
Laguna1 <lagunazoomarket@gmail.com>
2021-08-21 20:17:10 +0300

    Add Registrations controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,6 +2,7 @@
 
 class TweetsController < ApplicationController
   before_action :set_tweet, only: %i[show edit update destroy]
+  before_action :authenticate_user!, except: %i[index show]
 
   # GET /tweets or /tweets.json
   def index
