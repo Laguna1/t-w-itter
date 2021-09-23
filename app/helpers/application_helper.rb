ee7d38a9bc52b25555d77ea5327b62f6fed51b62
Laguna1 <lagunazoomarket@gmail.com>
2021-09-24 00:27:07 +0300

    Add follow&unfollow actions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def follow
+    @user = User.find(params[:id])
+    current_user.followees << @user
+    redirect_back(fallback_location: user_path(@user))
+  end
+
+  def unfollow
+    @user = User.find(params[:id])
+    current_user.followed_users.find_by(followee_id: @user.id).destroy
+    redirect_back(fallback_location: user_path(@user))
+  end
+
   def like_or_dislike_btn(tweet)
     like = Like.find_by(tweet: tweet, user: current_user)
     if like
