44de95862b883ebbcbdeb9c2cb89dede258c9bfa
Laguna1 <lagunazoomarket@gmail.com>
2021-09-24 00:24:30 +0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@
 Rails.application.routes.draw do
   resources :follows
   devise_for :users, controllers: { registrations: 'registrations' }
+  post '/users/:id/follow', to: 'users#follow', as: 'follow_user'
+  post '/users/:id/unfollow', to: 'users#unfollow', as: 'unfollow_user'
   resources :tweets do
     resources :likes, only: %i[create destroy]
   end
