0bffb8e150e3d960d2e8950652d640549eed968b
Laguna1 <lagunazoomarket@gmail.com>
2021-09-24 19:25:25 +0300

    Add indexes follower_id-followee_id-follows

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :trackable,
          :recoverable, :rememberable, :validatable
 
+  has_many :follows
   has_many :followed_users, foreign_key: :follower_id, class_name: 'Follow'
   has_many :followees, through: :followed_users
   has_many :following_users, foreign_key: :followee_id, class_name: 'Follow'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :follows
+
   devise_for :users, controllers: { registrations: 'registrations' }
+  resources :users do
+    resources :follows
+  end
   post '/users/:id/follow', to: 'users#follow', as: 'follow_user'
   post '/users/:id/unfollow', to: 'users#unfollow', as: 'unfollow_user'
   resources :tweets do
